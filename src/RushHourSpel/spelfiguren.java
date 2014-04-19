e826170af055f4cb9b97b7fb5c1c4f04b51a1471
Montoo <Montoo94@gmail.com>
2014-04-19 16:09:30 +0200

    Update spelfiguren.java

--- src/RushHourSpel/spelfiguren.java
+++ src/RushHourSpel/spelfiguren.java
@@ -1,5 +1,21 @@
 package RushHourSpel;
 
-public class spelfiguren {
+import javax.swing.JPanel;
+
+public class SpelFiguren extends JPanel {
+	
+		protected int lengthCarX;
+		protected int lengthCarY;
+		protected int lengthBusX;
+		protected int lengthBusY;
+		protected int LocationBusX = 300, LocationBusY, LocationRdX = 0, LocationRdY = 230, LocationBusX2 = 200, LocationBusY2;
+		
+		public SpelFiguren(){
+			
+			lengthCarX = 100;
+			lengthCarY = 40;
+			lengthBusX = 40;
+			lengthBusY = 150;
+		}
 
 }
